bb92ea78157c248b9d46a865cd2de8218a13ccd2
Evgeniy <evgeniy.goroh@gmail.com>
2016-05-27 18:04:48 +0400

    _call add

--- MObject.php
+++ MObject.php
@@ -5,6 +5,17 @@
 class MObject{
 	protected $_data = array();
 	
+	public function __call($method,$args){
+		$action = substr($method,0,3);
+		$key = substr($method,3);
+		switch ($action){
+			case 'get':
+				return $this->getData($key);
+			case 'set':
+				return $this->satdata($key, $args[0]);
+		}
+	}
+	
 	public function __construct($initialData){
 		$this->_data = $initialData;
 	}
